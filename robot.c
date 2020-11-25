1bde4a489bf24210a0a9cc697577bc39327dd6b8
chongb24 <chongb24@engineering04.campus.up.edu>
2020-11-24 17:10:38 -0800

    Optimized the robots' apperance. Added myself to the author list.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Ben Chong
  * @since August 2013
  *
  */
@@ -35,10 +35,10 @@ void robotPrintAscii(void)
   blank = blank +30;
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
-	 "%s    i_i    \n"
-	 "%s   [@_@]   \n"
+	 "%s    l_l    \n"
+	 "%s   [O_O]   \n"
 	 "%s  /|___|\\ \n"
-	 "%s   d   b   \n",blank, blank, blank, blank);
+	 "%s   0   0   \n",blank, blank, blank, blank);
 
   blank = blank - 10;
 }
@@ -55,7 +55,7 @@ void dalekPrintAscii(void)
   printf("\n\n"
 	 "I am Dalek, from Dr. Who\n"
 	 "%s      <----->      \n" 
-         "%s     <  (0)  >     \n"        
+         "%s     <  uwu  >     \n"        
          "%s     |       |     \n"
          "%s    < ------- >    \n"
          "%s    o         o    \n"
